11e0646c547bba4fc0ae9d0905a9c2db91211016
Haris Rahman <iharisrahman@yahoo.com>
2021-06-30 18:09:16 +0530

    added starter files, converted them to ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import MidArea from "./components/MidArea";
+import PreviewArea from "./components/PreviewArea";
 
-function App()
+export default function App()
 {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-				<a
-					className="App-link bg-red-800"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-        </a>
-			</header>
+		<div className="bg-blue-100 pt-6 font-sans">
+			<div className="h-screen overflow-hidden flex flex-row  ">
+				<div className="flex-1 h-screen overflow-hidden flex flex-row bg-white border-t border-r border-gray-200 rounded-tr-xl mr-2">
+					<Sidebar /> <MidArea />
+				</div>
+				<div className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
+					<PreviewArea />
+				</div>
+			</div>
 		</div>
 	);
 }
-
-export default App;
---
+++ src/components/Icon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Icon({ name, size = 20, className = "" }: { name: string, size: number, className: string })
+{
+	return (
+		<svg
+			className={`fill-current ${className}`}
+			width={size.toString() + "px"}
+			height={size.toString() + "px"}
+		>
+			<use xlinkHref={`/icons/solid.svg#${name}`} />
+		</svg>
+	);
+}
